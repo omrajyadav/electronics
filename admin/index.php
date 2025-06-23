a93a92ca2c9cdcda4256d262e0d66f829198201d
omrajyadav <omy696498@gmail.com>
2025-06-23 12:01:27 +0530

    same change

--- admin/index.php
+++ admin/index.php
@@ -35,14 +35,11 @@ include "sidebar.php";
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
 
-                <h4>CUSTOMER ADD</h4>
+                <h4>Category</h4>
               </div>
               <div class="icon">
-                <i class="ion ion-bag #"></i>
               </div>
-              <a href="customer add.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -50,14 +47,11 @@ include "sidebar.php";
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-                <p>city&state</p>
+                <p>product</p>
               </div>
               <div class="icon">
-                <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="cityadd.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -65,24 +59,20 @@ include "sidebar.php";
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
-
-                <p>state</p>
+                <h4>Order</h4>
+              </div>
               </div>
               <div class="icon">
-                <i class="ion ion-person-add"></i>
               </div>
-              <a href="stateadd.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-          <!-- ./col -->
 
-          <!-- ./col -->
         </div>
-        <!-- /.row -->
+      <a href="cart_add.php" class="btn px-0 ml-2">
+
       
-        <!-- /.row (main row) -->
-      </div><!-- /.container-fluid -->
+      </a>
+      </div>
     </section>
     <!-- /.content -->
   </div>

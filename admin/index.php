3e60f706f76e21f38a5a294fab302e839f240b91
omrajyadav <omy696498@gmail.com>
2025-06-25 10:56:42 +0530

    some change

--- admin/index.php
+++ admin/index.php
@@ -1,81 +1,11 @@
-
-<?php 
-include "header.php";
-?>
 <?php
+include "header.php";
+include "db_connection.php";
 include "sidebar.php";
 ?>
-<!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-            </ol>
-        
-          </div><!-- /.col -->
-          
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-4 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-
-                <h4>Category</h4>
-              </div>
-              <div class="icon">
-              </div>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-4 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-
-                <p>product</p>
-              </div>
-              <div class="icon">
-              </div>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-4 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h4>Order</h4>
-              </div>
-              </div>
-              <div class="icon">
-              </div>
-            </div>
-          </div>
-
-        </div>
-      <a href="cart_add.php" class="btn px-0 ml-2">
-
-      
-      </a>
-      </div>
-    </section>
-    <!-- /.content -->
-  </div>
- <?php 
- include "footer.php"
-  ?>
+<div>
+  
+</div>
+<?php
+include "footer.php";
+?>
